63f789cec3d5b503e783aade62bb284b4b5dc3bc
jfgomez1980 <gomezjosefernandocba@gmail.com>
2022-03-05 13:15:56 +0000

    Update BasicAppTest 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,7 +1,9 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import.postgresql.translation.meessage_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
